0d4b973295c1ead58c97816e0e8b7d8ea45045a9
TitoFlixa <t.hafez@flixalogix.com>
2019-05-31 18:06:19 +0000

    Div code

--- flixa_fashion/models/.ipynb_checkpoints/ff_division_code-checkpoint.py
+++ flixa_fashion/models/.ipynb_checkpoints/ff_division_code-checkpoint.py
@@ -8,7 +8,7 @@ from odoo import models, fields, api
 # 3 :  imports from odoo addons
 
 
-class ff_Division_Code(models.Model):
+class ffDivisionCode(models.Model):
     _name = 'ff_Division_Code'
     _description = 'Division Code'
 
