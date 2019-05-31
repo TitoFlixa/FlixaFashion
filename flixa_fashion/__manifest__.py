ab4c887f7b922f383810a26d56912fdc135bc8a7
TitoFlixa <t.hafez@flixalogix.com>
2019-05-31 17:25:56 +0000

    Div code

---
+++ flixa_fashion/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "FlixaFashion",
+
+    'summary': """
+        FlixaFashion, the complete business solution for the Fashion industry.""",
+
+    'description': """
+        The only system you will need to run your Fashion related business. Whether you are in Apprell, Accessories,
+        Footware, FlixaFashion got you covered. Includes the following:
+        
+        - E-Commerce
+        - Sales and Marketting business flow.
+        - Inventory Management.
+        - Purchasing and MFG.
+        
+        Please check the detailed features list at www.FlixaLogix.Com
+    """,
+
+    'author': "FlixaLogix Inc.",
+    'website': "http://www.FlixaLogix.com",
+    'category': 'Fashion ERP',
+    'version': '12.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/ff_division_code_views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ flixa_fashion/models/ff_division_code.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# 1 : imports of python lib
+
+# 2 :  imports of odoo
+from odoo import models, fields, api
+
+# 3 :  imports from odoo addons
+
+
+class ff_Division_Code(models.Model):
+    _name = 'ff_Division_Code'
+    _description = 'Division Code'
+
+    name = fields.Char('Division' , required = True)
+
+    
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
