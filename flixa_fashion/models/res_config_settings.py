a62887e8ff6efdf2dac3f3ff958e7f6287ab8b8a
TitoFlixa <t.hafez@flixalogix.com>
2019-06-04 03:56:22 +0000

    new setup form 3

--- flixa_fashion/models/res_config_settings.py
+++ flixa_fashion/models/res_config_settings.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _from odoo.exceptions import Warning
+from odoo import models, fields, api
+from odoo.exceptions import Warning
   class
 ResConfigSettings (models.TransientModel):
      _inherit = 'res.config.settings'
