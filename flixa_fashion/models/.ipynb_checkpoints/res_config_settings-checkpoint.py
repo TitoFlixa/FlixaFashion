822fccb0822a552acbce32902bb43543ca0b8450
TitoFlixa <t.hafez@flixalogix.com>
2019-06-05 01:58:59 +0000

    res model +

--- flixa_fashion/models/.ipynb_checkpoints/res_config_settings-checkpoint.py
+++ flixa_fashion/models/.ipynb_checkpoints/res_config_settings-checkpoint.py
@@ -12,13 +12,13 @@ class ResConfigSettings (models.TransientModel):
     
     _inherit = 'res.config.settings'
     module_use_season_codes = fields.Boolean ("Use Season Codes", default = False, help = "Associate your products with a specific season such as (Spring Collection, Summer Collection, etc.)")
-    module_mandatory_season_code = fields.Boolean ("Mandatory on Product Form", default = False, help = "Force the user to select a season code when adding a new product")
+    module_mandatory_season_code = fields.Boolean ("Season Mandatory on Product Form", default = False, help = "Force the user to select a season code when adding a new product")
     module__use_division_codes = fields.Boolean ("Use Division Codes", default = False, help = "Associate your products with a specific division code such as (Menswear, Women ware, Children, etc.)")
-    module_mandatory_division_code = fields.Boolean ("Mandatory on Product Form", default = False, help = "Force the user to select a division code when adding a new product")
+    module_mandatory_division_code = fields.Boolean ("Division Mandatory on Product Form", default = False, help = "Force the user to select a division code when adding a new product")
     module_use_classification_codes = fields.Boolean ("Use Classification Codes", default = False, help = "Associate your products with a specific Classification code such as (Pants, T Shirts, Accessories, etc.)")
-    module_mandatory_classification_code = fields.Boolean ("Mandatory on Product Form", default = False, help = "Force the user to select a classification code when adding a new product")
+    module_mandatory_classification_code = fields.Boolean ("Classification Mandatory on Product Form", default = False, help = "Force the user to select a classification code when adding a new product")
     module_use_designer_codes = fields.Boolean ("Use Designer Codes", default = False, help = "Associate your products with a specific designer.")
-    module_mandatory_designer_code = fields.Boolean ("Mandatory on Product Form", default = False, help = "Force the user to select a designer code when adding a new product")
+    module_mandatory_designer_code = fields.Boolean ("Designer Code Mandatory on Product Form", default = False, help = "Force the user to select a designer code when adding a new product")
     module_use_collection_codes = fields.Boolean ("Use Collection Codes", default = False, help = "Associate your products with a specific collection code.")
-    module_mandatory_collection_code = fields.Boolean ("Mandatory on Product Form", default = False, help = "Force the user to select a collection code when adding a new product")
+    module_mandatory_collection_code = fields.Boolean ("Collecion Code Mandatory on Product Form", default = False, help = "Force the user to select a collection code when adding a new product")
     
