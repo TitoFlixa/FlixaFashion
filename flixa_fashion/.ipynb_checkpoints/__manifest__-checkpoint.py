39c23246b7ccc5b2791059f3987d3acddcfcfc61
TitoFlixa <t.hafez@flixalogix.com>
2019-06-08 05:50:46 +0000

    analitics

--- flixa_fashion/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ flixa_fashion/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -23,7 +23,7 @@
     'version': '12.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_stock'],
+    'depends': ['base', 'product', 'analytics'],
 
     # always loaded
     'data': [
