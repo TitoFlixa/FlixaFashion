465a6d08137c6db23f51036e159505746b227165
TitoFlixa <t.hafez@flixalogix.com>
2019-06-09 14:54:59 +0000

    base + ana

--- flixa_fashion/__manifest__.py
+++ flixa_fashion/__manifest__.py
@@ -23,7 +23,7 @@
     'version': '12.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'analytic'],
+    'depends': ['base', 'analytic'],
 
     # always loaded
     'data': [
