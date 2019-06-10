8c4ca9c3869e3ff112a7ae76120a9c14f8516df4
TitoFlixa <t.hafez@flixalogix.com>
2019-06-10 17:30:19 +0000

    Flixa Base

--- flixa_fashion/__manifest__.py
+++ flixa_fashion/__manifest__.py
@@ -7,12 +7,12 @@
 
     'description': """
         The only system you will need to run your Fashion related business. Whether you are in Apprell, Accessories,
-        Footware, or anythung in between, FlixaFashion got you covered. Includes the following modules:
+        Footwear, or anything in between, FlixaFashion got you covered.  The base package includes the following modules:
         
-        - E-Commerce
-        - Sales and Marketting business flow.
-        - Inventory Management.
-        - Purchasing and MFG.
+        - Quote and complete Sales Management.
+        - Invoices and Customer Payment processes.
+        - Inventory Management with multi warehouse management.
+        - Analytics and Dashboards.
         
         Please check the detailed features list at www.FlixaLogix.Com
     """,
@@ -23,17 +23,12 @@
     'version': '12.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'analytic', 'sale_management'],
+    'depends': ['base', 'stock', 'account', 'sale_management', 'analytic'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/ff_menu.xml',
-        'views/ff_division_code_views.xml',
-        'views/ff_designer_code_views.xml',
-        'views/ff_collection_code_views.xml',
-        'views/ff_classification_code_views.xml',
-        'views/ff_season_code_views.xml',
         'views/ff_scale_code_views.xml',        
         'views/res_config_settings.xml',
         
--- flixa_fashion/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ flixa_fashion/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import ff_division_code
-from . import ff_designer_code
-from . import ff_classification_code
-from . import ff_collection_code
-from . import ff_season_code
 from . import ff_scale_code
 from . import res_config_settings
