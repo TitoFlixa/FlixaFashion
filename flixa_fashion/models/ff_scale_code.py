384936bcb6a2de2d9cd5c43eea90d178af0e011f
TitoFlixa <48025076+TitoFlixa@users.noreply.github.com>
2019-06-13 01:44:52 +0000

    new updated class2

--- flixa_fashion/models/ff_scale_code.py
+++ flixa_fashion/models/ff_scale_code.py
@@ -25,20 +25,14 @@ class ff_Scale_Code(models.Model):
         scales = super(ff_Scale_Code, self).create(vals_list)
         return scales
     
+    @api.multi
+    def write(self, vals_list):
+        super(ff_Scale_Code, self).write(vals_list)
+        return True
     
-	@api.multi
-	def write(self, vals_list):
-		super(ff_Scale_Code, self).write(vals_list)
-		return True
     
-
 # Inhirets the product.attribute.value class.    
 class ff_Size_Code(models.Model):
-	_inherit = 'product.attribute.value'
-	
-	is_size = fields.Boolean(related='attribute_id.is_scale')
-	sequence_rel = fields.Integer(related='sequence')
-
-
-    
-    
+    _inherit = 'product.attribute.value'
+    is_size = fields.Boolean(related='attribute_id.is_scale')
+    sequence_rel = fields.Integer(related='sequence')
