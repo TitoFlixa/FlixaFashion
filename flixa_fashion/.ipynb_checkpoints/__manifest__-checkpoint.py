dbbcc2dcde86f6ae442767cd5999b84437b4b5cb
TitoFlixa <48025076+TitoFlixa@users.noreply.github.com>
2019-06-27 03:22:36 +0000

    try

---
+++ flixa_fashion/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Flixa Logic Inc. All Rights Reserved.
+# App         : Flixa Fashion.
+# Desc        : Flixa_Fashion Manifest.
+# Comments    : Base Eddition - Includes Sales, Inoicing, Inventory, Analytics
+#################################################################################
+
+
+{
+    'name': "FlixaFashion",
+
+    'summary': """
+        FlixaFashion, the complete business solution for the Fashion industry.""",
+
+    'description': """
+        The only system you will need to run your Fashion related business. Whether you are in Apprell, Accessories,
+        Footwear, or anything in between, FlixaFashion got you covered.  The base package includes the following modules:
+        
+        - Quote and complete Sales Management.
+        - Invoices and Customer Payment processes.
+        - Inventory Management with multi warehouse management.
+        - Analytics and Dashboards.
+        
+        Please check the detailed features list at www.FlixaLogix.Com
+    """,
+
+    'author': "FlixaLogix Inc.",
+    'website': "http://www.FlixaLogix.com",
+    'category': 'Fashion ERP',
+    'version': '12.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'analytic', 'sale_stock', 'sale_management'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/ff_product_views.xml',
+        'views/ff_scale_code_views.xml',        
+        'views/res_config_settings.xml',
+    ],
+    
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
